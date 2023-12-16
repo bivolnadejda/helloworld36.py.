8bf1b1faa920179b525450677400ce56378c67dd
unknown <Nadia>
2023-12-16 23:45:23 +0200

    Primul commit: Adauga programul Hello World

---
+++ helloword36.py..py
@@ -0,0 +1,6 @@
+# Programul va afișa mesajul "Hello, World!" de 36 de ori.
+
+# Utilizăm un buclu for pentru a repeta afișarea mesajului.
+for i in range(36):
+    # Afișăm mesajul de fiecare dată când bucla se execută.
+    print("Hello, World!")
--- helloworld36.py
+++ helloworld36.py
@@ -1,2 +1,7 @@
-for _ in range(36):
- print("Hello World")
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Programul va afișa mesajul "Hello, World!" de 36 de ori.
+... 
+... # Utilizăm un buclu for pentru a repeta afișarea mesajului.
+... for i in range(36):
+...     # Afișăm mesajul de fiecare dată când bucla se execută.
